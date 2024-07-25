bca4b1f02b5c526d16c1d7209eb92e46538f6972
AramisB <brendachem7@gmail.com>
2024-07-25 14:30:13 +0300

    using nested loops on Pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,15 +10,16 @@ def pascal_triangle(n):
     """returns a list of lists of integers representing the
     Pascal’s triangle of n
     """
-    if n <= 0:
-        return []
-
     triangle = []
 
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            coefficient = factorial(i) / (factorial(j) * factorial(i - j))
-            row.append(int(coefficient))
+    if n <= 0:
+        return triangle
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        row = [1]
+        for i in range(len(triangle[-1]) - 1):
+            row.append(triangle[-1][i] + triangle[-1][i + 1])
+        row.append(1)
         triangle.append(row)
     return triangle
