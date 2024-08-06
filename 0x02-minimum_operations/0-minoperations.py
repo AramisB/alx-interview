d4148229116aa62042dcf4e94009d1708b84f4a4
AramisB <brendachem7@gmail.com>
2024-08-06 15:19:12 +0100

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,6 +2,8 @@
 """
 Minimal operations
 """
+
+
 def minOperations(n):
     """
     Determine the fewest number of operations needed
@@ -10,14 +12,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
