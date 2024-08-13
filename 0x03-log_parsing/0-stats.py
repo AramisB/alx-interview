b60cb60e835aec453945b4fd76ae3fe44fc463fa
AramisB <brendachem7@gmail.com>
2024-08-13 16:59:06 +0100

    a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-a script that reads stdin line by line and computes metrics
+A script that reads stdin line by line and computes metrics.
 """
-
 
 import sys
 import signal
@@ -11,7 +10,7 @@ import re
 
 def signal_handler(sig, frame):
     """
-    prints statistics and exits on CTRL+C
+    Handles SIGINT (CTRL+C) by printing the final statistics and exiting.
     """
     print_stats()
     sys.exit(0)
@@ -19,9 +18,9 @@ def signal_handler(sig, frame):
 
 def print_stats():
     """
-    Prints total file size and status code counts.
+    Prints the total file size and status code counts.
     """
-    print("File size:", total_size)
+    print(f"File size: {total_size}")
     for status_code in sorted(status_counts.keys()):
         if status_counts[status_code] > 0:
             print(f"{status_code}: {status_counts[status_code]}")
@@ -29,9 +28,7 @@ def print_stats():
 
 def process_line(line):
     """
-    Uses regular expression to extract IP, status code, and file size.
-    Converts status code and file size to integers.
-    Updates total size and status counts.
+    Processes a line from stdin to update metrics based on regex matches.
     """
     global total_size, status_counts, line_count
 
@@ -45,23 +42,38 @@ def process_line(line):
             status_code = int(status_code)
             file_size = int(file_size)
             total_size += file_size
-            status_counts[status_code] += 1
+            if status_code in status_counts:
+                status_counts[status_code] += 1
             line_count += 1
         except ValueError:
             pass
 
 
+# Initialize metrics
 total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                 403: 0, 404: 0, 405: 0, 500: 0}
+status_counts = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+    }
 line_count = 0
 
+# Set up signal handling
 signal.signal(signal.SIGINT, signal_handler)
 
-for line in sys.stdin:
-    process_line(line)
-    if line_count % 10 == 0:
-        print_stats()
-        line_count = 0
+try:
+    # Process each line from stdin
+    for line in sys.stdin:
+        process_line(line)
+        if line_count % 10 == 0:
+            print_stats()
+            line_count = 0
 
-print_stats()
+finally:
+    # Ensure final statistics are printed when the script exits
+    print_stats()
