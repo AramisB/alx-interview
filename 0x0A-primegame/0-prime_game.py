97800068293e5de63d8f5a2c6ace777ccabb1538
AramisB <brendachem7@gmail.com>
2024-10-01 15:07:24 +0300

    function to determine the winner of a prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,67 +1,77 @@
 #!/usr/bin/python3
 """
-Module for a prime game
+Prime Game: Maria and Ben's optimal prime picking game.
 """
 
 
 def sieve_of_eratosthenes(n):
     """
-    Helper function that returns a list of prime numbers up to n
-    using the Sieve of Eratosthenes algorithm.
+    Generate a list of booleans representing prime numbers up to n.
+    Args:
+        n (int): Number to count primes up to.
+    Returns:
+        list: List of booleans representing prime numbers up to n.
     """
     sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
+    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
+    for i in range(2, int(n**0.5) + 1):
         if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    return [i for i in range(2, n + 1) if sieve[i]]
+            for multiple in range(i * i, n + 1, i):
+                sieve[multiple] = False
+    return sieve
+
+
+def count_primes_up_to(n, sieve):
+    """
+    Count how many prime numbers exist up to a given number n.
+    Args:
+        n (int): Number to count primes up to.
+        sieve (list): List of booleans representing prime numbers up to n.
+    Returns:
+        int: Number of primes up to n.
+    """
+    if n < 2:
+        return 0
+    return sum(sieve[:n+1])
 
 
 def isWinner(x, nums):
     """
-    Determine the winner of the prime game
-    Args: x (int): number of rounds
-          nums (list): list of numbers
-    Returns: str: name of the winner, Ben or Maria
-             None if the winner cannot be determined
+    Determine the winner of the prime game.
+    Args:
+        x (int): Number of rounds.
+        nums (list): List of n values for each round.
+    Returns:
+        str: Name of the player ('Maria' or 'Ben').
+        None: If no clear winner.
     """
     if x < 1 or not nums:
         return None
-    # track number of wins
-    ben_wins = 0
-    maria_wins = 0
 
+    # Step 1: Precompute primes up to the maximum number in nums
+    # using Sieve of Eratosthenes
     max_num = max(nums)
+    sieve = sieve_of_eratosthenes(max_num)
 
-    # create list of primes up to max_num
-    prime_list = sieve_of_eratosthenes(max_num)
+    # Step 2: Track wins for Maria and Ben
+    maria_wins = 0
+    ben_wins = 0
 
-    for num in nums:
-        # copy the current set of numbers for the game round
-        game_set = list(range(1, num + 1))
-        # maria starts first
-        current_player = "Maria"
-        # track available numbers
-        while True:
-            available_primes = [prime for prime in prime_list
-                                if prime in game_set]
-            if not available_primes:
-                if current_player == "Maria":
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-            # remove the first available prime
-            # and its multiples in the game_set
-            prime = available_primes[0]
-            game_set = [num for num in game_set if num % prime != 0]
-            # switch players for the next game
-            current_player = "Ben" if current_player == "Maria" else "Maria"
+    # Step 3: Simulate each game round
+    for n in nums:
+        # Count how many primes are available in the current round
+        primes_count = count_primes_up_to(n, sieve)
 
-    if ben_wins > maria_wins:
-        return "Ben"
-    elif maria_wins > ben_wins:
-        return "Maria"
+        # If the number of primes is odd, Maria wins; if even, Ben wins
+        if primes_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    # Step 4: Determine the overall winner
+    if maria_wins > ben_wins:
+        return 'Maria'
+    elif ben_wins > maria_wins:
+        return 'Ben'
     else:
         return None
